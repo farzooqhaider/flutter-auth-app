050c0174c12b7d03028ee9fd6aac235ceb673392
farzooqhaider <haiderfarzooq@gmail.com>
2026-01-09 15:40:05 +0500

    Added validation and show/hide password

--- lib/login.dart
+++ lib/login.dart
@@ -2,17 +2,30 @@ import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
-
   @override
   State<Login> createState() => _Login();
 }
-
 class _Login extends State<Login> {
-
-
+  final _formKey = GlobalKey<FormState>();
+  final emailAddress = TextEditingController();
+  final passWord = TextEditingController();
+  bool _ispasswordVisible=false;
+  final snackBar = SnackBar(
+    content:Center( 
+    child: Text('Login Successful')),
+    behavior: SnackBarBehavior.floating,
+    duration: Duration(seconds: 2),
+    padding: EdgeInsets.all(40),
+    );
+  final snackBar1 = SnackBar(
+    content: Center(
+      child: Text('Invalid Email or Password'),),
+    behavior: SnackBarBehavior.floating,
+    duration: Duration(seconds: 2),
+    padding: EdgeInsets.all(40),
+    );  
   @override
-  Widget build(BuildContext context) {
-    
+  Widget build(BuildContext context) {  
     return Scaffold(
       backgroundColor: Colors.blueGrey[50],
       appBar: AppBar(
@@ -28,6 +41,8 @@ class _Login extends State<Login> {
       child:Center(
         child: Padding(
       padding: const EdgeInsets.all(20),
+      child:Form(
+        key: _formKey,
         child:Column(
         children: [  
           Text('Login',style: TextStyle(
@@ -36,28 +51,55 @@ class _Login extends State<Login> {
         color: Color.fromARGB(255, 32, 170, 224), 
       ),),
       const SizedBox(height: 40),
-         TextField(
+         TextFormField(
           keyboardType: TextInputType.text,
           decoration: const InputDecoration(
             prefixIcon: Icon(Icons.person),
             labelText: 'Email Address',
             hintText: 'example@you.com',
-            border: OutlineInputBorder(),
-            
-          ),  
+            border: OutlineInputBorder(), 
+          ),
+            validator: (value) {
+              if(value == null || value.isEmpty){
+                return 'Enter Email Address';
+              }
+              final bool emailValid = RegExp(
+                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
+              if(!emailValid){
+                return 'Invalid Email';
+              }
+              return null;  
+            },
          ),
          const SizedBox(height: 40),
-         TextField(
+         TextFormField(
           keyboardType: TextInputType.text,
           obscureText: true,
-          decoration: const InputDecoration(
+          decoration:  InputDecoration(
             prefixIcon: Icon(Icons.password_rounded),
             labelText: 'password',
             hintText: 'minimum 8 characters',
-            border: OutlineInputBorder(),
+            border: OutlineInputBorder(), 
+            suffixIcon: IconButton(onPressed: (){
+              setState(() {
+                _ispasswordVisible = !_ispasswordVisible;
+              });
+            }, 
+            icon:Icon( _ispasswordVisible ? Icons.visibility : Icons.visibility_off,)
             
-          ),  
-         ),
+          ),
+          ),
+          validator: (value) {
+            if(value == null || value.isEmpty){
+              return 'Enter Password'; 
+            }
+            if(value.length<8){
+              return 'Minimum 8 characters'; 
+            }
+            return null;
+          } ,
+          ), 
+        
          const SizedBox(height: 20),
          FractionallySizedBox(
                widthFactor: 0.8, 
@@ -67,15 +109,20 @@ class _Login extends State<Login> {
                   backgroundColor : Color.fromARGB(255, 32, 170, 224),
                 ),
                onPressed: () {
-                
+                if(_formKey.currentState!.validate()){
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }else{
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar1);
+                }
                },
                child: const Text('Login',style: TextStyle(
                 color: Colors.white,
                ),),
                ),
               )
+       
       ]
-        
+      )
       )
       )
       )
--- lib/main.dart
+++ lib/main.dart
@@ -20,28 +20,43 @@ class Signuppage extends StatefulWidget {
   const Signuppage({super.key});
 @override
 State<Signuppage> createState() => _Signuppage();
-
 }
-
 class _Signuppage extends State<Signuppage>{
+  final _formKey= GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool _ispasswordVisible= false;
+  final snackBar = SnackBar(
+                content: Center(
+                child: Text('Sign up successful')),
+                behavior: SnackBarBehavior.floating,
+                duration: Duration(seconds:2),
+                padding: EdgeInsets.all(20),);
+  final snackBar1 = SnackBar(
+                content:Center( 
+                child: Text('Invalid or Empty field')),
+                behavior: SnackBarBehavior.floating,
+                duration: Duration(seconds:2),
+                padding: EdgeInsets.all(20),);
  @override
   Widget build(BuildContext context){
-    
     return Scaffold(
       backgroundColor: Colors.blueGrey[50],
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 32, 170, 224),
-        title: Center(
-          child:Text('Flutter Auth App',style: TextStyle(
+        centerTitle: true,
+        title:Text('Flutter Auth App',style: TextStyle(
             color: Color.fromARGB(255, 255, 255, 255)
           ),
           ),
           ),
-      ),
+     
        body: SingleChildScrollView(
        child:Center(
         child: Padding(
       padding: const EdgeInsets.all(20),
+      child : Form(
+        key: _formKey,
         child:Column(
         children: [  
           Text('Sign Up',style: TextStyle(
@@ -50,59 +65,105 @@ class _Signuppage extends State<Signuppage>{
         color: Color.fromARGB(255, 32, 170, 224), 
       ),),
       const SizedBox(height: 40),
-         TextField(
+         TextFormField(
           keyboardType: TextInputType.text,
           decoration: const InputDecoration(
             prefixIcon: Icon(Icons.person),
             labelText: 'First Name',
             hintText: 'First Name',
-            border: OutlineInputBorder(),
-            
+            border: OutlineInputBorder(), 
           ),
+          validator: (value){
+            if(value==null || value.isEmpty){
+              return 'Enter your name';
+              
+            }else{
+              return null;
+            }
+          },
          ),
           const SizedBox(height: 40),
-         TextField(
+         TextFormField(
           keyboardType: TextInputType.text,
           decoration: const InputDecoration(
             prefixIcon:Icon(Icons.person),
             labelText: 'Last Name',
             hintText: 'Last Name',
-            border: OutlineInputBorder(),
-            
+            border: OutlineInputBorder(), 
           ),
+          validator: (value){
+            if(value==null || value.isEmpty){
+              return 'Enter your name';
+            }else{
+              return null;
+            }
+          },
          ),
       const SizedBox(height: 40),
-         TextField(
+         TextFormField(
           keyboardType: TextInputType.emailAddress,
           decoration: const InputDecoration(
             prefixIcon: Icon(Icons.email),
             labelText: 'Email Address',
             hintText: 'example@you.com',
             border: OutlineInputBorder(),
-            
-          ),  
+          ),
+          validator: (value) {
+            if(value == null || value.isEmpty){
+              return 'Enter Email Address'; 
+            }
+            final bool validEmail = RegExp(
+              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"
+              ).hasMatch(value);
+            if (!validEmail) {
+              return 'Please enter a valid email address';
+             }
+             return null;
+          },  
          ),
          const SizedBox(height: 40),
-         TextField(
+         TextFormField(
           keyboardType: TextInputType.text,
-          obscureText: true,
-          decoration: const InputDecoration(
+          obscureText: !_ispasswordVisible,
+          decoration:  InputDecoration(
             prefixIcon: Icon(Icons.password_rounded),
             labelText: 'password',
             hintText: 'minimum 8 characters',
             border: OutlineInputBorder(),
-            
-          ),  
+            suffixIcon: IconButton(onPressed:() {
+              setState(() {
+                   _ispasswordVisible = !_ispasswordVisible;
+                      });
+                  },
+            icon:Icon(
+              _ispasswordVisible ? Icons.visibility :Icons.visibility_off,
+            ),) 
+          ),
+
+          validator: (value) {
+          if(value==null || value.isEmpty){
+            return 'Enter password';
+          }
+          if(value.length < 8){
+            return 'password must be atleast 8 characters';
+          }
+          return null;
+          },     
          ),
          const SizedBox(height: 20),
          FractionallySizedBox(
                widthFactor: 0.8, // 0.8 means 80% of the parent's width
                child: ElevatedButton(
                onPressed: () {
+                if(_formKey.currentState!.validate()){
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);  
                 Navigator.pushReplacement(context,
                 MaterialPageRoute(builder:(context) => Login()
                 )
                 );
+                }else{
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar1);
+                }
                },
                style: ElevatedButton.styleFrom(
                 backgroundColor: Color.fromARGB(255, 32, 170, 224),
@@ -116,10 +177,17 @@ class _Signuppage extends State<Signuppage>{
               FractionallySizedBox(
                 widthFactor: 0.8,
           child:TextButton(onPressed: (){
+            if(_formKey.currentState!.validate()){
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
             Navigator.pushReplacement(context,
              MaterialPageRoute(builder: (context)=>Login()
+             
              )
              );
+             
+            }else{
+              ScaffoldMessenger.of(context).showSnackBar(snackBar1);
+            }
           },
           child:Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -132,18 +200,16 @@ class _Signuppage extends State<Signuppage>{
             fontWeight: FontWeight.bold,
             )
             )
-          
         ]
           ),
           ),
         ),  
         ]
         ),
-        )
-       )
-      
+        ),
+        ),
+       ),
        ),  
     );
   }
-
 }
